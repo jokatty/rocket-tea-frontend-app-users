42ffc1e79c5a2da8d2fd4d01efe2cf66474a8ac1
eddiejpot <jamesedwardporter@gmail.com>
2021-08-27 00:37:52 +0800

    added config file to store backend path

--- src/components/ViewOrders/Orders.jsx
+++ src/components/ViewOrders/Orders.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getOrderDetails } from '../../StoreLogic/store';
 import OrderDetails from './OrderDetails';
+import { SocketContext, socket } from '../../context/Socket';
 
 export default function Orders() {
   // local state
@@ -10,13 +11,23 @@ export default function Orders() {
   const [showOrderDetails, setShowOrderDetails] = useState(false);
 
   useEffect(() => {
+    console.log('re-rendering');
     // fetch orders on page load
-    (async () => {
+    const fetchData = async () => {
       const userId = 1;
       const response = await getOrderDetails(userId);
       console.log(response.data);
       // setOrderDetails(response);
-    })();
+    };
+    fetchData();
+    // ================================================= SOCKET MVP
+    // For MVP Socket updates all user's app
+    // Goal is to have socket only update the relavant user's app
+    socket.on('ORDER-ACCEPTED', (message) => {
+      console.log(message);
+      fetchData();
+    });
+    // ================================================= SOCKET MVP
   }, []);
 
   return (
